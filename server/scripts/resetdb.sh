b36c8aee96fb3a517f6e84701a5afb34844f2a30
Moshi <29588163+musukwamoshi@users.noreply.github.com>
2023-02-18 22:28:48 +0200

    modified build commands

--- server/scripts/resetdb.sh
+++ server/scripts/resetdb.sh
@@ -1,7 +1,7 @@
-dropdb --if-exists curateddocs
-psql -d postgres -c "DROP USER IF EXISTS curateddocs_app"
-createdb curateddocs
-psql -d curateddocs -c "ALTER DATABASE curateddocs SET TIMEZONE TO 'Etc/UTC';"
-psql -d curateddocs -c "CREATE USER curateddocs_app WITH PASSWORD 'greenbullsucks@2023';"
-psql -d curateddocs -c "GRANT ALL PRIVILEGES ON DATABASE curateddocs TO curateddocs_app;"
-psql -d curateddocs -c "ALTER USER curateddocs_app WITH SUPERUSER"
+dropdb --if-exists briefdocs
+psql -d postgres -c "DROP USER IF EXISTS briefdocs_app"
+createdb briefdocs
+psql -d briefdocs -c "ALTER DATABASE briefdocs SET TIMEZONE TO 'Etc/UTC';"
+psql -d briefdocs -c "CREATE USER briefdocs_app WITH PASSWORD 'greenbullsucks@2023';"
+psql -d briefdocs -c "GRANT ALL PRIVILEGES ON DATABASE curateddocs TO briefdocs_app;"
+psql -d briefdocs -c "ALTER USER briefdocs_app WITH SUPERUSER"
